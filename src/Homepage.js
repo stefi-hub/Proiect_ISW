84d301347062131af3ec9c119493e5fa26fdd192
tohaneanovidiu <126237180+tohaneanovidiu@users.noreply.github.com>
2024-03-26 19:56:40 +0200

    Update Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,65 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Homepage.css';
-import logoIcon from './p1.jpg';
-import cartIcon from './p1.jpg';
-import slideShowImage1 from './p1.jpg';
-import slideShowImage2 from './p2.jpg';
-import slideShowImage3 from './p3.jpg';
+import logoIcon from './img/p1.jpg';
+import slideShowImage1 from './img/p2.jpg';
+import slideShowImage2 from './img/p3.jpg';
+import slideShowImage3 from './img/p4.jpg';
+import slideShowImage4 from './img/p5.jpg';
 
 const Homepage = ({ logout }) => {
+
+  const imageLinks = [slideShowImage1, slideShowImage2, slideShowImage3, slideShowImage4];
+  const [currentIndices, setCurrentIndices] = useState([0, 1, 2]);
+
+  const prevSlide = () => {
+  setCurrentIndices(currentIndices.map(index =>
+    (index - 1 + imageLinks.length) % imageLinks.length
+  ));
+};
+
+const nextSlide = () => {
+  setCurrentIndices(currentIndices.map(index =>
+    (index + 1) % imageLinks.length
+  ));
+};
+
+
   return (
-    <div className="homepage">
-      <div className="top-bar">
-        <span className="contact">CONTACT</span>
-        <img src={logoIcon} alt="Logo" className="logo" />
-        <button onClick={logout} className="logout-button">LOGOUT</button>
+      <div className="homepage">
+
+          <div className="logo-section">
+              <img src={logoIcon} alt="Logo" className="logo"/>
+          </div>
+
+          <div className="topbar">
+              <ul className="top-links">
+                  <li><a href="/contact" style={{color: 'white'}}>Contact us</a></li>
+                  <li><a href="/about" style={{color: 'white'}}>About us</a></li>
+                  <li><a href="/shops" style={{color: 'white'}}>Shops</a></li>
+                  <li><a href="/shoppingcart" style={{color: 'white'}}>Shopping Cart</a></li>
+                  <li><a href="/login" style={{color: 'white'}}>Logout</a></li>
+              </ul>
+          </div>
+
+          <div className="slideshow-container">
+              <button className="arrow prev" onClick={prevSlide}>&lt;</button>
+              <div className="slides">
+                  {currentIndices.map((index) => (
+                      <a
+                          key={index}
+                          href="#" // Acesta este locul unde veți adăuga linkul corespunzător fiecărei imagini
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="slide-link"
+                          style={{
+                              margin: '0 5px',
+                              display: 'block',
+                              flex: '0 0 calc(33.333% - 10px)',
+                              borderRadius: '8px'
+                          }}
+                      >
+                          <img
+                              src={imageLinks[index]}
+                              alt={`Slide ${index + 1}`}
+                              className="slide active"
+                              style={{
+                                  width: '100%',
+                                  height: 'auto',
+                                  borderRadius: '8px'
+                              }}
+                          />
+                      </a>
+                  ))}
+              </div>
+
+
+              <button className="arrow next" onClick={nextSlide}>&gt;</button>
+          </div>
+
+          <div className="Our-categories">
+
+          </div>
+
+          <div className="Accesories">
+
+          </div>
+
+          <div className="social-media">
+              <span>Twitter</span>
+              <span>Instagram</span>
+              <span>Facebook</span>
+          </div>
+
+          <div className="points-of-interest">
+              <div className="point1">Subscribe to our Newsletter</div>
+              <div className="point2">Featured Images</div>
+          </div>
+
       </div>
-
-      <div className="navbar">
-        <ul className="nav-links">
-          <li>HOME</li>
-          <li>PRODUCTS</li>
-          <li>TUTORIALS</li>
-          <li>SHOPS</li>
-          <li>ABOUT-US</li>
-          <li>CONTACT-US</li>
-        </ul>
-        <img src={cartIcon} alt="Shopping Cart" className="cart-icon" />
-      </div>
-
-      <div className="slideshow-container">
-        {}
-        <div className="slide fade">
-          <img src={slideShowImage1} alt="Offer 1" />
-        </div>
-        <div className="slide fade">
-          <img src={slideShowImage2} alt="Offer 2" />
-        </div>
-        <div className="slide fade">
-          <img src={slideShowImage3} alt="Offer 3" />
-        </div>
-      </div>
-
-      <div className="content">
-        {}
-
-        {}
-
-        {}
-      </div>
-
-      <div className="newsletter-section">
-        <div className="newsletter">Subscribe to our Newsletter</div>
-        <div className="pictures">Featured Images</div>
-      </div>
-
-      <div className="social-media">
-        {}
-        <span>Twitter</span>
-        <span>Instagram</span>
-        <span>Facebook</span>
-      </div>
-    </div>
   );
 };
 
