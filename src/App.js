9f9d99e83e6bb86c8aec6f210254281231a40292
tohaneanovidiu <126237180+tohaneanovidiu@users.noreply.github.com>
2024-03-29 16:07:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Register from './register';
 import Homepage from './Homepage';
+import ProductPage from './ProductPage';
+import Guitars from './guitars';
+import Pianos from './pianos';
+import Tutorials from './tutorials';
+import Sheets from './sheets';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [showRegister, setShowRegister] = useState(false);
 
-  const handleLoginSuccess = (token) => {
-    localStorage.setItem('token', token);
+  const handleLogin = () => {
     setIsLoggedIn(true);
+    console.log('merge');
   };
-
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    setIsLoggedIn(false);
-  };
-
-  const toggleRegister = () => {
-    setShowRegister(!showRegister);
-  };
-
-  if (isLoggedIn) {
-    return <Homepage logout={handleLogout} />;
-  }
 
   return (
+    <Router>
       <div className="App">
-        <div className="background-image"></div>
-        {!showRegister ? (
-            <Login onLogin={handleLoginSuccess} onShowRegister={toggleRegister}/>
-        ) : (
-            <Register onShowLogin={toggleRegister}/>
-        )}
+        <Routes>
+          <Route path="/" element={<Login onLogin={handleLogin} />} /> {}
+          <Route path="/register" element={<Register />} /> {}
+          <Route path="/homepage" element={<Homepage />} />
+          {}
+          {isLoggedIn ? (
+            <Route path="/homepage" element={<Homepage />} /> // aici adaugi paginile care pot fi accesate doar dupa login
+          ) : (
+            <Route path="/homepage" element={<Login onLogin={handleLogin} />} />
+          )}
+          <Route path="/product/:productName" element={<ProductPage />} />
+          <Route path="/guitars" element={<Guitars />} />
+          <Route path="/pianos" element={<Pianos />} />
+          <Route path="/tutorials" element={<Tutorials />} />
+          <Route path="/sheets" element={<Sheets />} />
+        </Routes>
       </div>
+    </Router>
   );
 }
 
