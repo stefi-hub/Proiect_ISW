a23dac02940c3cd43e04313bf6af4ed09cad64d6
EmanuelCata <126237261+EmanuelCata@users.noreply.github.com>
2024-04-16 12:19:01 +0300

    Update ProductPage.js

--- src/ProductPage.js
+++ src/ProductPage.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import { PRODUCTS } from './ProductsPage';
+import { useCart } from './CartContext';
 import './ProductPage.css';
 
 const ProductPage = () => {
-  const { productName } = useParams();
-  const product = PRODUCTS.find(p => p.name.toLowerCase() === productName.replace(/-/g, ' '));
+  const { productName } = useParams();  
+  const product = PRODUCTS.find(p => p.name.toLowerCase() === productName.replace(/-/g, ' '));  
+  const { addToCart } = useCart();
 
-  if (!product) {
-    return <div>Product not found</div>;
-  }
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleAddToCart = () => {
+    addToCart(product);
+    setShowMessage(true);
+    setTimeout(() => setShowMessage(false), 3000);
+    console.log('Product added to cart:', product);
+  };
 
   return (
     <div className="product-page-container">
       <div className="product-container">
+        <Link to="/homepage">Inapoi</Link>
         <img src={product.image} alt={product.name} className="product-image" />
         <h1 className="product-name">{product.name}</h1>
         <div className="price-and-cart">
           <p className="product-price">{`Price: $${product.price}`}</p>
-          <button className="add-to-cart-btn">Add to Cart 🛒</button>
+          <button className="add-to-cart-btn" onClick={handleAddToCart}>Add to Cart 🛒</button>
         </div>
         <p className="product-description">{product.description}</p>
+        <div className={`cart-message ${showMessage ? 'show' : 'hide'}`}>Item added to your cart!</div>
       </div>
     </div>
   );
